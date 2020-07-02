cd92bcea1681e41c8cfe8eda7eb235872b62064e
Phil.Feng <phil.feng@homecreditcfc.cn>
2020-07-02 13:20:08 +0800

    sha256

--- FZHCache/FZHCache/Source/DiskStorage.swift
+++ FZHCache/FZHCache/Source/DiskStorage.swift
@@ -26,7 +26,7 @@ private extension String {
     var sha256: String {
         let utf8 = cString(using: .utf8)
         var digest = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
-        CC_SHA224(utf8, CC_LONG(utf8!.count - 1), &digest)
+        CC_SHA256(utf8, CC_LONG(utf8!.count - 1), &digest)
         return digest.reduce("") { $0 + String(format:"%02x", $1) }
     }
 }
